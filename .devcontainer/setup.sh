58a5ccb832fce7863ae9372c4f907501a3569157
Chris Wyatt <9978862+chriswyatt1@users.noreply.github.com>
2026-03-03 14:49:50 +0000

    Add docker api bashrc

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -5,6 +5,8 @@ echo "export PROMPT_DIRTRIM=2" >> $HOME/.bashrc
 echo "export PS1='\[\e[3;36m\]\w ->\[\e[0m\\] '" >> $HOME/.bashrc
 export PROMPT_DIRTRIM=2
 export PS1='\[\e[3;36m\]\w ->\[\e[0m\\] '
+# Add docker api version, that the defult is broken
+echo "export DOCKER_API_VERSION=1.43" >> $HOME/.bashrc
 
 # Update Nextflow
 nextflow self-update
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nf-core-pipeline-setup
 Use nf-core tools to create a pipeline
+
+You need to set `export DOCKER_API_VERSION=1.43` in Codespaces
